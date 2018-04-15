78870644ee2bc36240c424eca211943b17464f20
Bona <bjorn.fyrvall@gmail.com>
2018-04-15 19:39:29 +0200

    Fixed design flaw in home page. Added some air for the View all button.

--- Application/Views/Home/Index.php
+++ Application/Views/Home/Index.php
@@ -41,36 +41,10 @@
     </div>
 <?php endforeach;?>
 
-<div class="row">
+<div class="row my-4">
     <div class="col-lg-12">
         <a href="/history" class="btn btn-success btn-md">
             View all posts
         </a>
     </div>
 </div>
-
-<!--
-<div class="card my-4">
-    <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-    <div class="card-body">
-        <a href="/home/post">
-            <h2 class="card-title">Post Title</h2>
-        </a>
-        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-        <a href="#" class="btn btn-success">Read More &rarr;</a>
-    </div>
-    <div class="card-footer text-muted">
-        Posted on January 1, 2017 by
-        <a href="#">Start Bootstrap</a>
-    </div>
-</div>
-
-<ul class="pagination justify-content-center mb-4">
-    <li class="page-item">
-        <a class="page-link" href="#">&larr; Older</a>
-    </li>
-    <li class="page-item disabled">
-        <a class="page-link" href="#">Newer &rarr;</a>
-    </li>
-</ul>
--->
